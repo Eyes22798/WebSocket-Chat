78fccb19cf1df1ee5d32d25412b077dfc44fbc2a
Eyes22798 <eyes22798@gmail.com>
2021-05-04 19:32:43 +0800

    refactor: 完善vite.config配置

---
+++ src/components/avatarChoose/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="avatar-choose-cmp all0">
+    <div class="avatar-list hor-ver-center" :style="device === 'Mobile' ? {width: '90%'} : {}">
+      <i class="el-icon-close p-r-t" @click="close"></i>
+      <span class="p-l-t">选择头像</span>
+      <div
+        class="avatar-item box-shadow1"
+        v-for="(item, index) in avatarList"
+        :key="index"
+        @click="choose('face/'+item)"
+      >
+        <img width="80" height="80" :src="IMG_URL + 'face/' + item" alt="" srcset="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'AvatarChoose',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.avatar-choose-cmp {
+  position: fixed;
+  z-index: 1004;
+  background-color: rgba(0, 0, 0, .2);
+  .avatar-list {
+    width: 504px;
+    height: 350px;
+    padding: 40px 20px 20px;
+    background-color: #ffffff;
+    overflow-x: hidden;
+    border-radius: 5px;
+    .avatar-item {
+      box-sizing: border-box;
+      display: inline-block;
+      margin: 5px;
+      border: 1px solid #DCDFE6;
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
-import { defineConfig } from 'vite'
+import { UserConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { loadEnv } from "./build/utils"
+import { createProxy } from "./build/proxy"
+
+const pathResolve = (dir: string): any => {
+  return resolve(__dirname, ".", dir)
+}
+
+const alias: Record<string, string> = {
+  "/@": pathResolve("src"),
+  //解决开发环境下的警告 You are running the esm-bundler build of vue-i18n. It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
+  "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
+}
+
+const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_OPEN } = loadEnv()
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define: {
-    'process.env': {}
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+  
+  return {
+    root,
+    base: process.env.NODE_ENV === "production" ? "/manages/" : VITE_PUBLIC_PATH,
+    plugins: [vue()],
+    resolve: {
+      alias
+    },
+    server: {
+      // 是否开启 https
+      https: false,
+      /**
+       * 端口号
+       * @default 3000
+       */
+      proxy: createProxy(VITE_PROXY),
+      port: VITE_PORT
+    },
   }
-})
+}
