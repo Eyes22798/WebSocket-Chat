9becd3e54c1339aa2893fd594b594dbbaa749292
Eyes22798 <eyes22798@gmail.com>
2021-08-04 21:02:45 +0800

    refactor: 处理缩进

--- src/router/index.ts
+++ src/router/index.ts
@@ -53,8 +53,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 router.beforeEach((to, from, next) => {
